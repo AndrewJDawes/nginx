5b93f6fc29864d9da23d931e4d458a47b095a6ec
Andrew Dawes <andrewjamesdawes@gmail.com>
2022-12-24 16:19:00 -0500

    Add DNS resolver to NGINX conf so it can use Docker networks for DNS resolution

--- start.sh
+++ start.sh
@@ -2,10 +2,17 @@
 if [ -f "getenv.sh" ]; then
     source getenv.sh
 fi
+
+# Attach to nginx-reverse-proxy network
+network_name="nginx-reverse-proxy"
+network_cmd="docker network inspect $network_name || docker network create $network_name"
+eval "$network_cmd"
+
 eval_cmd="docker run \
 -d \
 --restart=unless-stopped \
 --name $PROJECT_DOCKER_CONTAINER_NAME \
+--network=$network_name \
 -p 80:80 \
 -p 443:443 \
 -v "$(pwd)"/data/etc/nginx/sites:/etc/nginx/sites:ro \
