4952ced0b49e01e67d9f6c4ce0be4a19ac310c65
Andrew Dawes <andrewjamesdawes@gmail.com>
2022-12-26 13:33:07 -0500

    Actually run cron in background

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -2,5 +2,5 @@
 # Generate global certs, dummy certs, start cron to issue/renew certs, start nginx in foreground
 /app/scripts/global_certs.sh && \
 /app/scripts/site_certs.sh && \
-crond && \
+crond -b -L /app/cron.log && \
 exec nginx -g "daemon off;"
