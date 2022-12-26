fa112d837cc8eb4937f7659e8598266fba77339e
Andrew Dawes <andrewjamesdawes@gmail.com>
2022-12-26 13:27:49 -0500

    Fix cron to daemonize

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Generate global certs, dummy certs, then start nginx in foreground
+# Generate global certs, dummy certs, start cron to issue/renew certs, start nginx in foreground
 /app/scripts/global_certs.sh && \
 /app/scripts/site_certs.sh && \
+crond && \
 exec nginx -g "daemon off;"
